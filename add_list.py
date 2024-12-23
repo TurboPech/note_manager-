4f1bad561ab0234ff01246030159f14f0b4ca6a2
TurboPech <87541671+TurboPech@users.noreply.github.com>
2024-12-23 15:31:18 +0300

    Create add_list.py

---
+++ add_list.py
@@ -0,0 +1,8 @@
+zagolovok1=input('Заголовок 1 :')
+zagolovok2=input('Заголовок 2 :')
+zagolovok3=input('Заголовок 3 :')
+zagolovki=[zagolovok1,zagolovok2,zagolovok3]
+print(zagolovki)
+print(zagolovok1)
+print(zagolovok2)
+print(zagolovok3)
